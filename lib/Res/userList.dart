21eca7e488c7bf889beea4d712b4edb974734dd7
BadRat-in <rbmorena42@gmail.com>
2021-10-03 11:14:24 +0530

    First Update

---
+++ lib/Res/userList.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+class ChatUsers {
+  String name;
+  String message;
+  String imageUrl;
+  String time;
+
+  ChatUsers(
+      {@required this.name,
+      @required this.message,
+      @required this.imageUrl,
+      @required this.time});
+}
--- lib/Screens/home.dart
+++ lib/Screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:secure_chat/Res/userList.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -6,24 +7,83 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  @override
+  List<ChatUsers> userList = [
+    ChatUsers(
+        name: "Rahul",
+        message: "Hello",
+        imageUrl: "assets/user.jpg",
+        time: "Now"),
+    ChatUsers(
+        name: "Sumat Singh",
+        message:
+            "What are you Doing dfhjs fegkhsdfsdkhj  sdfljsdfsfljsdglodhj dnhfkgdighd ijoif h",
+        imageUrl: "assets/login.png",
+        time: '17 Mar'),
+    ChatUsers(
+        name: 'Shivani Patel',
+        message:
+            'Lets go fdkjgdfg dfsdkhfs sdfoijdesr   erth9iu kfhjsdfthew wejhrdosi',
+        imageUrl: 'assets/user.jpg',
+        time: 'Today')
+  ];
   int _active = 1;
+  TextEditingController searchUser = TextEditingController();
+  bool isSearch = false;
   Widget setScreen;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    searchUser.addListener(() {
+      print(searchUser.text);
+    });
+  }
+
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Chat Seguro"),
-        actions: [
-          IconButton(
-              onPressed: () {
-                Navigator.pushReplacementNamed(context, "signin");
-              },
-              icon: Icon(
-                Icons.logout,
-                size: 30,
-              ))
-        ],
-      ),
+          title: isSearch ? createSearchfield() : Text("Chat Seguro"),
+          leading: isSearch
+              ? IconButton(
+                  onPressed: () {
+                    setState(() {
+                      isSearch = false;
+                    });
+                  },
+                  icon: Icon(Icons.arrow_back))
+              : null,
+          actions: [
+            isSearch
+                ? IconButton(
+                    icon: Icon(Icons.cancel),
+                    iconSize: 28,
+                    onPressed: () {
+                      setState(() {
+                        if (searchUser.text.length > 0) {
+                          searchUser.clear();
+                        } else {
+                          isSearch = false;
+                        }
+                      });
+                    },
+                  )
+                : IconButton(
+                    onPressed: () {
+                      setState(() {
+                        isSearch = true;
+                      });
+                    },
+                    icon: Icon(Icons.search),
+                    iconSize: 28,
+                    color: Colors.white,
+                  ),
+            SizedBox(
+              width: 8,
+            )
+          ]),
       bottomNavigationBar: Container(
         height: 70,
         decoration: BoxDecoration(color: Colors.white),
@@ -48,31 +108,28 @@ class _HomeState extends State<Home> {
                   style: TextStyle(
                       color: _active == 1 ? Colors.blue[900] : Colors.blueGrey,
                       fontWeight:
-                      _active == 1 ? FontWeight.bold : FontWeight.w500,
+                          _active == 1 ? FontWeight.bold : FontWeight.w500,
                       fontSize: _active == 1 ? 20 : 0),
                   duration: const Duration(milliseconds: 200))
-            ]),
-            Row(children: [
+            ]),Row(children: [
               IconButton(
                   onPressed: () {
                     setState(() {
                       _active = 2;
-                      setScreen = _storyContrainer(context);
+                      setScreen = _addUserContainer(context);
                     });
                   },
                   icon: Icon(
-                    _active == 2
-                        ? Icons.video_collection
-                        : Icons.video_collection_outlined,
+                    _active == 2 ? Icons.add : Icons.add_outlined,
                     size: 35,
                     color: _active == 2 ? Colors.blue[900] : Colors.blueGrey,
                   )),
               AnimatedDefaultTextStyle(
-                  child: Text("Story"),
+                  child: Text("New Chat"),
                   style: TextStyle(
                       color: _active == 2 ? Colors.blue[900] : Colors.blueGrey,
                       fontWeight:
-                      _active == 2 ? FontWeight.bold : FontWeight.w500,
+                          _active == 2 ? FontWeight.bold : FontWeight.w500,
                       fontSize: _active == 2 ? 20 : 0),
                   duration: const Duration(milliseconds: 200))
             ]),
@@ -81,21 +138,45 @@ class _HomeState extends State<Home> {
                   onPressed: () {
                     setState(() {
                       _active = 3;
-                      setScreen = _settingsContrainer(context);
+                      setScreen = _storyContrainer(context);
                     });
                   },
                   icon: Icon(
-                    _active == 3 ? Icons.settings : Icons.settings_outlined,
+                    _active == 3
+                        ? Icons.video_collection
+                        : Icons.video_collection_outlined,
                     size: 35,
                     color: _active == 3 ? Colors.blue[900] : Colors.blueGrey,
                   )),
               AnimatedDefaultTextStyle(
-                  child: Text("Settings"),
+                  child: Text("Story"),
                   style: TextStyle(
                       color: _active == 3 ? Colors.blue[900] : Colors.blueGrey,
                       fontWeight:
                           _active == 3 ? FontWeight.bold : FontWeight.w500,
                       fontSize: _active == 3 ? 20 : 0),
+                  duration: const Duration(milliseconds: 200))
+            ]),
+            Row(children: [
+              IconButton(
+                  onPressed: () {
+                    setState(() {
+                      _active = 4;
+                      setScreen = _settingsContrainer(context);
+                    });
+                  },
+                  icon: Icon(
+                    _active == 4 ? Icons.settings : Icons.settings_outlined,
+                    size: 35,
+                    color: _active == 4 ? Colors.blue[900] : Colors.blueGrey,
+                  )),
+              AnimatedDefaultTextStyle(
+                  child: Text("Settings"),
+                  style: TextStyle(
+                      color: _active == 4 ? Colors.blue[900] : Colors.blueGrey,
+                      fontWeight:
+                          _active == 4 ? FontWeight.bold : FontWeight.w500,
+                      fontSize: _active == 4 ? 20 : 0),
                   duration: const Duration(milliseconds: 200))
             ]),
           ],
@@ -105,13 +186,71 @@ class _HomeState extends State<Home> {
     );
   }
 
-  Widget _homeContrainer(context) {
-    return ListView(
 
+  Widget createSearchfield() {
+    return TextField(
+      cursorColor: Colors.white70,
+      style: TextStyle(
+          color: Colors.white,fontSize: 18
+      ),
+      decoration: InputDecoration(
+          focusColor: Colors.white38,
+          prefixIcon: Icon(
+            Icons.search,
+            color: Colors.white70,
+          ),
+          hintText: "Search User",
+          hintStyle: TextStyle(color: Colors.white, fontSize: 18)),
+      autofocus: true,
+      controller: searchUser,
     );
   }
 
+
+  Widget _homeContrainer(context) {
+    return ListView.builder(
+        itemCount: userList.length,
+        shrinkWrap: true,
+        padding: EdgeInsets.only(top: 10),
+        physics: NeverScrollableScrollPhysics(),
+        itemBuilder: (context, index) {
+          return ListTile(
+              leading: CircleAvatar(
+                backgroundImage: AssetImage(userList[index].imageUrl),
+                maxRadius: 30,
+              ),
+              title: Text(
+                userList[index].name,
+                style: TextStyle(fontSize: 18, color: Colors.white),
+              ),
+              subtitle: Text(
+                (userList[index].message.length <= 40)
+                    ? userList[index].message
+                    : 'message',
+                style: TextStyle(
+                    color: Colors.white70,
+                    fontSize: 14,
+                    fontWeight: (index == 0 || index == 3)
+                        ? FontWeight.bold
+                        : FontWeight.normal),
+              ),
+              trailing: Text(
+                userList[index].time,
+                style: TextStyle(
+                    color: Colors.white54,
+                    fontSize: 14,
+                    fontWeight: (index == 0 || index == 3)
+                        ? FontWeight.bold
+                        : FontWeight.normal),
+              ));
+        });
+  }
+
   Widget _storyContrainer(context) {
+    return Container();
+  }
+
+  Widget _addUserContainer(context){
     return Container();
   }
 
